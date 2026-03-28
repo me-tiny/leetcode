edcfd9a72558759b599bb9c52839fbbb20c90fe3
Metin Yalcinkaya <metinyalcinkaya@hotmail.com.au>
2026-03-29 10:12:44 +1100

    0242-valid-anagram

---
+++ problems/0242-valid-anagram/sol.cpp
@@ -0,0 +1,40 @@
+#include "lc.hpp"
+
+class Solution {
+  public:
+    bool isAnagram(std::string s, std::string t) {
+        if (s.length() != t.length())
+            return false;
+
+        std::unordered_map<char, int> freq;
+
+        for (char c : s) {
+            freq[c]++;
+        }
+
+        for (char c : t) {
+            freq[c]--;
+        }
+
+        for (auto c : freq) {
+            if (c.second != 0)
+                return false;
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+
+    // test cases
+    std::string s1 = "anagram";
+    std::string t1 = "nagaram";
+    expect(s.isAnagram(s1, t1), true);
+
+    std::string s2 = "rat";
+    std::string t2 = "car";
+    expect(s.isAnagram(s1, t1), true);
+
+    return 0;
+}
